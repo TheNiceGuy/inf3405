f0781b04f385ac3f92512019896159a5bc92fab8
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2017-10-30 15:00:40 -0400

    Document Database class

--- Server/Server/Database.cpp
+++ Server/Server/Database.cpp
@@ -15,8 +15,17 @@ Database::~Database() {
     /* TODO: implement this method */
 }
 
+bool Database::save(const string& file) const {
+    return false;
+}
+
 void Database::addMsg(struct msg_server_text* msg) {
     /* TODO: implement this method */
+}
+
+User* Database::addUser(const std::string& name, const std::string& pass) {
+    /* TODO: implement this method */
+    return nullptr;
 }
 
 User* Database::getUser(const string& name) const {
--- Server/Server/Database.h
+++ Server/Server/Database.h
@@ -5,22 +5,73 @@
 #include <string>
 #include "User.h"
 
+/**
+ * This class defines the database of users and messages.
+ */
 class Database {
 public:
+    /**
+     * Default constructor.
+     */
     Database();
 
+    /**
+     * Values contructor.
+     *
+     * @param file The file of the database.
+     */
     Database(const std::string& file);
 
+    /**
+     * Destructor.
+     */
     ~Database();
 
+    /**
+     * This method saves the database into a file.
+     *
+     * @param file The name of the file.
+     *
+     * @return If the database has been saved, then `true`, else `false`.
+     */
+    bool save(const std::string& file) const;
+
+    /**
+     * This method adds a new message into the backlog.
+     *
+     * @param msg The new message to add.
+     */
     void addMsg(struct msg_server_text* msg);
 
+    /**
+     * This method adds a new user to the database.
+     *
+     * @param name The name of the user.
+     * @param pass The password of the user.
+     *
+     * @return The user that has been created.
+     */
+    User* addUser(const std::string& name, const std::string& pass);
+
+    /*************
+     * Accessors *
+     *************/
+
+    /**
+     * This method returns a user in the database.
+     *
+     * @param name The name of the user.
+     *
+     * @return The requested user.
+     */
     User* getUser(const std::string& name) const;
 
-
 private:
+    /** The list of users in the database. */
     std::vector<User> users_;
-    std::vector<std::string> backlog_;
+
+    /** The list of messages that has been sent. */
+    std::vector<struct msg_server_text*> backlog_;
 };
 
 #endif
